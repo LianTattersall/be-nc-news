ceb016eb7475a6b9eafe86a20cc84653949e6b7e
Lian Tattersall <liantattersall@Lians-iMac.lan>
2024-07-15 16:21:39 +0100

    add change to fetchArticles func

--- models/articles-models.js
+++ models/articles-models.js
@@ -6,17 +6,9 @@ exports.fetchArticleById = (article_id) => {
 
 exports.fetchArticles = () => {
     return db.query(`
-    SELECT articles.article_id , articles.title , articles.author , articles.topic , articles.votes , articles.article_img_url , articles.created_at , COUNT(comment_id) AS comments_count FROM articles 
+    SELECT articles.article_id , articles.title , articles.author , articles.topic , articles.votes , articles.article_img_url , articles.created_at , CAST(COUNT(comment_id) AS INT) AS comments_count FROM articles 
     LEFT JOIN comments 
     ON comments.article_id = articles.article_id
     GROUP BY articles.article_id , articles.title , articles.author , articles.topic , articles.votes , articles.article_img_url , articles.created_at
     ORDER BY created_at DESC`)
-    .then(({rows}) => {
-        const articles = rows.map((article) => {
-            const articleCopy = {...article}
-            articleCopy.comments_count = Number(article.comments_count)
-            return articleCopy
-        })
-        return articles
-    })
 }
