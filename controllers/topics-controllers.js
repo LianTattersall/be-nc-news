a477ee16e2c7e360a0960ef8eccc21c45cb2a849
Lian Tattersall <liantattersall@Lians-iMac.lan>
2024-07-15 11:26:14 +0100

    add controller for GET /api/topics

---
+++ controllers/topics-controllers.js
@@ -0,0 +1,11 @@
+const { fetchTopics } = require("../models/topics-models")
+
+exports.getTopics = (request , response , next) => {
+    fetchTopics()
+    .then(({rows}) => {
+        response.status(200).send({topics: rows})
+    })
+    .catch((err) => {
+        next(err)
+    })
+}
