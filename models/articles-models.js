9b4975669abde26c66345290634bd02c0ab9ae45
Lian Tattersall <liantattersall@Lians-iMac.lan>
2024-07-16 14:11:01 +0100

    add model updateArticle

--- models/articles-models.js
+++ models/articles-models.js
@@ -12,3 +12,17 @@ exports.fetchArticles = () => {
     GROUP BY articles.article_id , articles.title , articles.author , articles.topic , articles.votes , articles.article_img_url , articles.created_at
     ORDER BY created_at DESC`)
 }
+
+exports.updateArticle = (article_id , inc_votes) => {
+    return db.query(`
+    UPDATE articles 
+    SET votes = votes + $1
+    WHERE article_id = $2 RETURNING *` , [inc_votes , article_id])
+    .then(({rows}) => {
+        if (rows.length === 0) {
+            return Promise.reject({status: 404, msg: '404 - Article not found'})
+        } else {
+            return {rows}
+        }
+    })
+}
