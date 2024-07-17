340a2a5f3aa0a62bf1681a03e90053d75c2f984e
Lian Tattersall <liantattersall@Lians-iMac.lan>
2024-07-17 11:54:11 +0100

    modify article for topic query

--- models/articles-models.js
+++ models/articles-models.js
@@ -1,18 +1,28 @@
 const db = require('../db/connection.js')
+const { checkTopicExists } = require('../model-utils.js')
 
 exports.fetchArticleById = (article_id) => {
     return db.query(`SELECT * FROM articles WHERE article_id = $1` , [article_id])
 }
 
-exports.fetchArticles = (sort_by = 'created_at' , order = 'desc') => {
+exports.fetchArticles = (sort_by = 'created_at' , order = 'desc' , topic) => {
     let queryStr = `
     SELECT articles.article_id , articles.title , articles.author , articles.topic , articles.votes , articles.article_img_url , articles.created_at , 
-    CAST(COUNT(comment_id) AS INT) AS comments_count FROM articles 
+    CAST(COUNT(comment_id) AS INT) AS comment_count FROM articles 
     LEFT JOIN comments 
     ON comments.article_id = articles.article_id
-    GROUP BY articles.article_id , articles.title , articles.author , articles.topic , articles.votes , articles.article_img_url , articles.created_at`
+    `
+    const queryArr = []
 
-    const validColumns = ['article_id' , 'title' , 'topic' , 'author' , 'created_at' , 'votes' , 'article_img_url' , 'comments_count']
+    const queryValues = []
+    if (topic) {
+        queryStr += ` WHERE articles.topic = $1` 
+        queryValues.push(topic)
+        queryArr.push(checkTopicExists(topic))
+    }
+    queryStr += ` GROUP BY articles.article_id , articles.title , articles.author , articles.topic , articles.votes , articles.article_img_url , articles.created_at`
+    
+    const validColumns = ['article_id' , 'title' , 'topic' , 'author' , 'created_at' , 'votes' , 'article_img_url' , 'comment_count']
     if (!validColumns.includes(sort_by)) {
         return Promise.reject({status: 400 , msg: '400 - Bad Request Invalid Column Name'})
     }
@@ -23,7 +33,16 @@ exports.fetchArticles = (sort_by = 'created_at' , order = 'desc') => {
     }
     queryStr += ` ORDER BY ${sort_by} ${order}`
 
-    return db.query(queryStr)
+    const articlesQuery = db.query(queryStr , queryValues)
+    queryArr.unshift(articlesQuery)
+    
+    return Promise.all(queryArr)
+    .then(([{rows} , topicExists]) => {
+        if (rows.length === 0 && !topicExists) {
+            return Promise.reject({status: 404 , msg: '404 - Topic not found'})
+        }
+        return {rows}
+    })
 }
 
 exports.updateArticle = (article_id , inc_votes) => {
