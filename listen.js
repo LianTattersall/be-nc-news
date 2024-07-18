98c395d5b16f63d8088d997b5c6e5dfce3f29d2c
Lian Tattersall <liantattersall@Lians-iMac.lan>
2024-07-18 13:44:21 +0100

    update listen.js

--- listen.js
+++ listen.js
@@ -3,6 +3,6 @@ const app = require('./app.js')
 const { PORT = 9090 } = process.env;
 
 app.listen(PORT , () => {
-        return console.log('listening on port 9090')
+        console.log('listening on port 9090')
     }
 )
