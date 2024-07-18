e8ea7f44b24d87426f7770722dc7243d1016a5e4
Lian Tattersall <liantattersall@Lians-iMac.lan>
2024-07-18 15:44:51 +0100

    add controller getUserByUsername

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -1,9 +1,20 @@
-const { fetchUsers } = require("../models/users-models")
+const { fetchUsers, fetchUserByUsername } = require("../models/users-models")
 
 exports.getUsers = (request , response , next) => {
     fetchUsers()
     .then(({rows}) => {
         response.status(200).send({users: rows})
+    })
+    .catch((err) => {
+        next(err)
+    })
+}
+
+exports.getUserByUsername = (request , response , next) => {
+    const {username} = request.params
+    fetchUserByUsername(username)
+    .then(({rows}) => {
+        response.status(200).send({user: rows[0]})
     })
     .catch((err) => {
         next(err)
