86c8f71fc9261d49897d2dea7c39e9dac6e2a421
Lian Tattersall <liantattersall@Lians-iMac.lan>
2024-07-19 16:02:54 +0100

    add model addTopic

--- models/topics-models.js
+++ models/topics-models.js
@@ -3,3 +3,7 @@ const db = require('../db/connection.js')
 exports.fetchTopics = () => {
     return db.query(`SELECT * FROM topics`)
 }
+
+exports.addTopic = (slug , description) => {
+    return db.query(`INSERT INTO topics (slug , description) VALUES ($1 , $2) RETURNING *` , [slug ,description])
+}
