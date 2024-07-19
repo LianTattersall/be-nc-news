40366e202c760a28996fa5a3f6997ba00aa8be7d
Lian Tattersall <liantattersall@Lians-iMac.lan>
2024-07-19 16:35:11 +0100

    add delete article endpoint

--- routers/articles-routers.js
+++ routers/articles-routers.js
@@ -1,4 +1,4 @@
-const { getArticles, getArticleById, patchArticle, postArticle } = require("../controllers/articles-controllers");
+const { getArticles, getArticleById, patchArticle, postArticle, deleteArticleById } = require("../controllers/articles-controllers");
 const { getCommentsByArticleId, postComment } = require("../controllers/comments-controllers");
 const articlesRouter = require("express").Router();
 
@@ -11,6 +11,7 @@ articlesRouter
 .route('/:article_id')
 .get(getArticleById)
 .patch(patchArticle)
+.delete(deleteArticleById)
 
 articlesRouter
 .route('/:article_id/comments')
