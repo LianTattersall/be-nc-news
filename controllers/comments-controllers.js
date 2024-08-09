d64c00bf7facfddf8218ee99d71eccd9b85b20c1
Lian Tattersall <liantattersall@Lians-iMac.lan>
2024-08-09 10:42:07 +0100

    add offset query to GET comments endpoint

--- controllers/comments-controllers.js
+++ controllers/comments-controllers.js
@@ -1,48 +1,53 @@
-const { fetchCommentsByArticleId, addComment, removeComment, updateComment } = require("../models/comments-models")
+const {
+  fetchCommentsByArticleId,
+  addComment,
+  removeComment,
+  updateComment,
+} = require("../models/comments-models");
 
-exports.getCommentsByArticleId = (request , response , next) => {
-    const {article_id} = request.params
-    const {limit , p} = request.query
-    fetchCommentsByArticleId(article_id , limit , p)
-    .then(({rows}) => {
-        response.status(200).send({comments: rows})
+exports.getCommentsByArticleId = (request, response, next) => {
+  const { article_id } = request.params;
+  const { limit, p, offset } = request.query;
+  fetchCommentsByArticleId(article_id, limit, p, offset)
+    .then(({ rows }) => {
+      response.status(200).send({ comments: rows });
     })
     .catch((err) => {
-        next(err)
-    })
-}
+      next(err);
+    });
+};
 
-exports.postComment = (request , response , next) => {
-    const {username , body} = request.body
-    const {article_id} = request.params
-    addComment(username , body , article_id)
-    .then(({rows}) => {
-        response.status(201).send({comment: rows[0]})
+exports.postComment = (request, response, next) => {
+  const { username, body } = request.body;
+  const { article_id } = request.params;
+  addComment(username, body, article_id)
+    .then(({ rows }) => {
+      response.status(201).send({ comment: rows[0] });
     })
     .catch((err) => {
-        next(err)
-    })
-}
+      next(err);
+    });
+};
 
-exports.deleteComment = (request , response , next) => {
-    const {comment_id} = request.params
-    removeComment(comment_id)
+exports.deleteComment = (request, response, next) => {
+  const { comment_id } = request.params;
+  removeComment(comment_id)
     .then(() => {
-        response.status(204).send()
+      response.status(204).send();
     })
     .catch((err) => {
-        next(err)
-    })
-}
+      next(err);
+    });
+};
 
-exports.patchComment = (request , response , next) => {
-    const {inc_votes} = request.body
-    const {comment_id} = request.params
-    updateComment(comment_id , inc_votes)
-    .then(({rows}) => {
-        response.status(200).send({comment: rows[0]})
+exports.patchComment = (request, response, next) => {
+  const { inc_votes } = request.body;
+  const { comment_id } = request.params;
+  updateComment(comment_id, inc_votes)
+    .then(({ rows }) => {
+      response.status(200).send({ comment: rows[0] });
     })
     .catch((err) => {
-        next(err)
-    })
-}
+      next(err);
+    });
+};
--- models/comments-models.js
+++ models/comments-models.js
@@ -1,51 +1,69 @@
-const { checkArticleExists } = require('../model-utils.js')
-const db = require('../db/connection.js')
+const { checkArticleExists } = require("../model-utils.js");
+const db = require("../db/connection.js");
 
-exports.fetchCommentsByArticleId = (article_id , limit = 10 , p = 1) => {
-    let queryStr = `SELECT * FROM comments WHERE article_id = $1 ORDER BY created_at DESC`
+exports.fetchCommentsByArticleId = (article_id, limit = 10, p = 1, offset) => {
+  let queryStr = `SELECT * FROM comments WHERE article_id = $1 ORDER BY created_at DESC`;
 
-    if (!Number(limit)) {
-        return Promise.reject({status: 400 , msg: '400 - Limit is not a number'})
+  if (!Number(limit)) {
+    return Promise.reject({ status: 400, msg: "400 - Limit is not a number" });
+  }
+  if (!Number(p)) {
+    return Promise.reject({ status: 400, msg: "400 - Page is not a number" });
+  }
+  if (!Number(offset) && offset !== undefined) {
+    return Promise.reject({ status: 400, msg: "400 - Offset is not a number" });
+  }
+  if (offset) {
+    queryStr += ` LIMIT ${limit} OFFSET ${offset}`;
+  } else {
+    queryStr += ` LIMIT ${limit} OFFSET ${(p - 1) * limit}`;
+  }
+
+  const commentsQuery = db.query(queryStr, [article_id]);
+  const articleExistsQuery = checkArticleExists(article_id);
+  return Promise.all([commentsQuery, articleExistsQuery]).then(
+    ([commentsQuery, articleExists]) => {
+      if (articleExists) {
+        return commentsQuery;
+      } else {
+        return Promise.reject({ status: 404, msg: "404 - No such article" });
+      }
     }
-    if(!Number(p)) {
-        return Promise.reject({status: 400 , msg: '400 - Page is not a number'})
-    }
-    queryStr += ` LIMIT ${limit} OFFSET ${(p - 1) * limit}`
+  );
+};
 
-    const commentsQuery = db.query(queryStr , [article_id])
-    const articleExistsQuery = checkArticleExists(article_id)
-    return Promise.all([commentsQuery , articleExistsQuery])
-    .then(([commentsQuery , articleExists]) => {
-        if (articleExists) {
-            return commentsQuery
-        } else {
-            return Promise.reject({status: 404, msg: '404 - No such article'})
-        }
-    })
-}
-
-exports.addComment = (username , body , article_id) => {
-    return db.query(`
+exports.addComment = (username, body, article_id) => {
+  return db.query(
+    `
     INSERT INTO comments (body , article_id , author) 
-    VALUES ($1 , $2 , $3) RETURNING *` , [body , article_id , username])
-}
+    VALUES ($1 , $2 , $3) RETURNING *`,
+    [body, article_id, username]
+  );
+};
 
 exports.removeComment = (comment_id) => {
-    return db.query(`DELETE FROM comments WHERE comment_id = $1 RETURNING *` , [comment_id])
-    .then(({rows}) => {
-        if (rows.length === 0) {
-            return Promise.reject({status: 404 , msg: '404 - Comment not found'})
-        }
-    })
-}
+  return db
+    .query(`DELETE FROM comments WHERE comment_id = $1 RETURNING *`, [
+      comment_id,
+    ])
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({ status: 404, msg: "404 - Comment not found" });
+      }
+    });
+};
 
-exports.updateComment = (comment_id , inc_votes) => {
-    return db.query(`
-    UPDATE comments SET votes = votes + $1 WHERE comment_id = $2 RETURNING *` , [inc_votes , comment_id])
-    .then(({rows}) => {
-        if (rows.length === 0) {
-            return Promise.reject({status: 404 , msg: '404 - Comment not found'})
-        }
-        return {rows}
-    })
-}
+exports.updateComment = (comment_id, inc_votes) => {
+  return db
+    .query(
+      `
+    UPDATE comments SET votes = votes + $1 WHERE comment_id = $2 RETURNING *`,
+      [inc_votes, comment_id]
+    )
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({ status: 404, msg: "404 - Comment not found" });
+      }
+      return { rows };
+    });
+};
